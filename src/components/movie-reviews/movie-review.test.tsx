9a62a356e9eae429d97bdb8125e800e17a9e17cd
Alexander Kachar <alexkachar.github@gmail.com>
2020-08-25 23:19:24 +0300

    eslint fixes

--- src/components/movie-reviews/movie-review.test.tsx
+++ src/components/movie-reviews/movie-review.test.tsx
@@ -1,8 +1,6 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
-import {Provider} from 'react-redux';
 import MovieReviews from './movie-reviews';
-import mockStore from '../../test-data/mock-store';
 import Review from '../../interfaces/review';
 import mockReviews from '../../test-data/mock-reviews';
 
